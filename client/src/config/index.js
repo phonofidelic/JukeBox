088e58b3527455af4708d070a0e819be596abb5f
Christopher Clemons <chris.clemons85@gmail.com>
2018-05-12 20:11:39 -0700

    move theme configuration object to app config
    Styles configuration object is imported from config and
    passed to createMuiTheme().

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,41 @@
 export const URLS = {
 	TRACKS_URL: '/tracks'
 }
+
+export const THEME = {
+	typography: {
+    // Use the system font over Roboto.
+    fontFamily:
+      '-apple-system,system-ui,BlinkMacSystemFont,' +
+      '"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif',
+    fontWeightMedium: 500,
+    body1: {
+      fontWeight: 500,
+    },
+    subheading: {
+      fontSize: 12,
+    },
+    button: {
+      // fontStyle: 'italic',
+      // size: 'large'
+    },
+  },
+  palette: {
+    primary: {
+      main: '#d2d8d8',
+      light: '#fff',
+      dark: '#222'
+    },
+    secondary: {
+      main: '#f44336',
+      light: '#ff7961',
+      dark: '#ba000d'
+    },
+    context: {
+      danger: '#EF5350',
+      warning: 'orange',
+      success: 'green',
+      info: 'blue'
+    }
+  }
+}
