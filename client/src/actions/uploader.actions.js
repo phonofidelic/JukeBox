2d850417bca4ea73d94427cbda51a007312a59f1
phonofidelic <chris_clemons85@hotmail.com>
2018-11-17 03:30:01 -0800

    add call to idbTrack.setAll on UPLOAD_SUCCESS
    Updates IndexedDB with newly added track data.
    BUG: data is only updated on the device that initiated the upload.
    Need to add a way to check for fresh data from network DB and update
    IndexedDB when data is out of sync.

--- client/src/actions/uploader.actions.js
+++ client/src/actions/uploader.actions.js
@@ -4,6 +4,7 @@ import {
 	UPLOAD_FAILURE,
 	SET_MESSAGE
 } from '../actiontypes';
+import { idbTrack } from '../utils/idbUtils';
 import axios from 'axios';
 import { URLS } from '../config';
 
@@ -11,19 +12,21 @@ const TRACKS_URL = URLS.TRACK_URL;
 
 export const uploadTracks = (formData) => {
 	console.log('@uploadTracks, formData:', formData.getAll('audioFiles'));
-	return dispatch => {
+	return async (dispatch) => {
 		dispatch({
 			type: UPLOAD_TRACKS
 		});
-		axios.post(TRACKS_URL, formData, { 
-			headers: { 
-				token: localStorage.getItem('JWT'),
-				userId: localStorage.getItem('userId')
-			}
-		})
-		.then(response => {
+		let response;
+		try {
+			response = await axios.post(TRACKS_URL, formData, { 
+				headers: { 
+					token: localStorage.getItem('JWT'),
+					userId: localStorage.getItem('userId')
+				}
+			});
 			console.log('uploadTrack response:', response);
 			// document.dispatchEvent(new Event('library-update'));
+			idbTrack.setAll(response.data.tracks);
 			dispatch({
 				type: UPLOAD_SUCCESS,
 				uploadedTracks: response.data.tracks
@@ -36,15 +39,41 @@ export const uploadTracks = (formData) => {
 					context: 'success'
 				}
 			});
-		})
-		.catch(err => {
-			console.error('uploadTrack error:', err);
+		} catch(err) {
+			console.error(err);
 			dispatch({
 				type: UPLOAD_FAILURE,
 				data: err.response.data,
 				status: err.response.status ,
 				message: err.response.data.message || err.response.data, 
 			});
-		});
+		}
+
+
+		// .then(response => {
+		// 	console.log('uploadTrack response:', response);
+		// 	// document.dispatchEvent(new Event('library-update'));
+		// 	dispatch({
+		// 		type: UPLOAD_SUCCESS,
+		// 		uploadedTracks: response.data.tracks
+		// 	});
+		// 	dispatch({
+		// 		type: SET_MESSAGE,
+		// 		message: {
+		// 			//	TODO: Store target response data in constant?
+		// 			text: `${response.data.message}`, 
+		// 			context: 'success'
+		// 		}
+		// 	});
+		// })
+		// .catch(err => {
+		// 	console.error('uploadTrack error:', err);
+		// 	dispatch({
+		// 		type: UPLOAD_FAILURE,
+		// 		data: err.response.data,
+		// 		status: err.response.status ,
+		// 		message: err.response.data.message || err.response.data, 
+		// 	});
+		// });
 	}
 }
