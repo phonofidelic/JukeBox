0c639ddf59c32849e0fd83d89af574eb5c2c6bbd
Christopher Clemons <chris.clemons85@gmail.com>
2018-12-11 10:54:59 -0800

    add screenshots to cypress sample_spec
    Trying out automated screenshots in sample_spec.js
    added cypress/screenshots dir to .gitignore

--- client/cypress/integration/sample_spec.js
+++ client/cypress/integration/sample_spec.js
@@ -1,7 +1,18 @@
 describe('First sample test', () => {
 	it('goes to the login page', () => {
 		cy.visit('http://localhost:3000');
-		cy.get('[data-cy=signInReveal]').click();
+		cy.screenshot();
+	});
+	
+	it('opens the sign in form', () => {
+		cy.get('[data-cy=signInReveal]').click();	
+		cy.wait(300);
+		cy.screenshot();
+	});
+		
+	it('opnes the registration form', () => {
 		cy.get('[data-cy=registrationInReveal]').click();
+		cy.wait(300);
+		cy.screenshot();
 	});
 });
