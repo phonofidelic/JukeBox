bd3ce9e47c5917d366a31e329129c1331d52e4ec
Christopher Clemons <chris.clemons85@gmail.com>
2018-12-13 11:28:23 -0800

    add cypress-axe a11y testing plugin and axe-core a11y engine
    cypress-axe: https://github.com/avanslaars/cypress-axe
    axe-core: https://github.com/dequelabs/axe-core

--- client/cypress/integration/sample_spec.js
+++ client/cypress/integration/sample_spec.js
@@ -1,18 +1,24 @@
 describe('First sample test', () => {
 	it('goes to the login page', () => {
 		cy.visit('http://localhost:3000');
-		cy.screenshot();
+		cy.injectAxe();
+		// cy.screenshot();
 	});
+
+	it('Has no detectable a11y violations on load', () => {
+	  // Test the page at initial load
+	  cy.checkA11y()
+	})
 	
 	it('opens the sign in form', () => {
 		cy.get('[data-cy=signInReveal]').click();	
-		cy.wait(300);
-		cy.screenshot();
+		// cy.wait(300);
+		// cy.screenshot();
 	});
 		
 	it('opnes the registration form', () => {
 		cy.get('[data-cy=registrationInReveal]').click();
-		cy.wait(300);
-		cy.screenshot();
+		// cy.wait(300);
+		// cy.screenshot();
 	});
 });
