64f7b329fdcd6a0096f5fce8897506d49a409970
Christopher Clemons <chris.clemons85@gmail.com>
2019-01-06 18:25:33 -0800

    add: createLibFolder ptivate user controller method
    createLibFolder checks users Google Drive account for a folder named
    _Jukebox_library, creating it if it does not exist, and returns a
    Google Drive file object with the shape:
    
    {
    	kind: String,
    	id: String,
    	name: String,
    	mimeType: String
    }

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -3,12 +3,48 @@ const User = require('../models/user.model');
 const {google} = require('googleapis');
 const inspectConfig = {colors: true, depth: null};
 
-STRINGS = {
+const STRINGS = {
 	gdAuthConfirmationTitle: 'Google Drive authorization confirmation',
 	gdAuthConfirmationMessage: 'Jukebox has been authorized to store audio files on your Google Drive account.'
 };
 
+const GD_LIB_FOLDER = '_Jukebox_library';
+
 let oauth2Client;
+
+const createLibFolder = async (oauth2Client, next) => {
+	/***
+	 *	returns:
+	 *	{ 
+	 *		kind: String,
+	 *		id: String,
+	 *		name: String,
+	 *		mimeType: String
+	 *	}
+	 */
+	const drive = google.drive({
+		version: 'v3',
+		auth: oauth2Client
+	});
+	const checkFolderRes = await drive.files.list({
+		q: `name="${GD_LIB_FOLDER}"`
+	});
+	
+	// Check if audio library folder already exists
+	if (checkFolderRes.data.files.length > 0) return checkFolderRes.data.files[0];
+	
+	console.log('*** CREATING NEW FOLDER ***')
+	const res = await drive.files.create({
+	  requestBody: {
+	    name: GD_LIB_FOLDER,
+	    mimeType: 'application/vnd.google-apps.folder'
+	  },
+	  media: {
+	    mimeType: 'application/vnd.google-apps.folder'
+	  }
+	});
+	return res;
+};
 
 // GET /gdrive/authURL
 module.exports.getGDriveAuthURL = (req, res, next) => {
@@ -30,34 +66,29 @@ module.exports.getGDriveAuthURL = (req, res, next) => {
 };
 
 // GET /gdrive/authcode
-module.exports.gdOauthcallback = async (req, res, next) => {
-	console.log('gdOauthcallback:', req.query);
-	
+module.exports.gdOauthcallback = async (req, res, next) => {	
 	const { code, userId } = req.query;
-
 	const { tokens } = await oauth2Client.getToken(code);
 	oauth2Client.setCredentials(tokens);
 
 	console.log('gdAuth token:', util.inspect(tokens, inspectConfig));
-
 	// Save tokens to User doc in DB
 	await User.findByIdAndUpdate(userId, { gdTokenData: tokens }, { new: true }, (err, updatedUserData) => {
+		// TODO: if err, respond with error page
 		if (err) return next(err);
 		console.log('Successfully updated users Google Drive token data:', updatedUserData);
 	});
 
-	// TODO: respond with success/failure message
+	// Create 'Jukebox library' folder in users Drive account
+	const gdFolder = await createLibFolder(oauth2Client, next);
+	console.log('createLibFolder, gdFolder:', util.inspect(gdFolder, inspectConfig))
+
 	res.status(200).render('gdAuthConfirmation', 
 		{
 			title: STRINGS.gdAuthConfirmationTitle, 
 			message: STRINGS.gdAuthConfirmationMessage 
 		}
-		// (err, html) => {
-		// 	if (err) return next(err);
-		// 	res.send(html);
-		// }
 	);
-	// res.status(200).json({message: 'auth code recieved', ...req.query})
 };
 
 module.exports.test_gdAuthConfirmationPage = (req, res, next) => {
@@ -67,4 +98,4 @@ module.exports.test_gdAuthConfirmationPage = (req, res, next) => {
 			message: STRINGS.gdAuthConfirmationMessage 
 		}
 	);
-}
+};
