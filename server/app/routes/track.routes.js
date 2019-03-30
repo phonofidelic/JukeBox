70cfa638c4eebfc7320dc0326c838bc658c21ef6
phonofidelic <chris_clemons85@hotmail.com>
2019-03-30 11:15:01 -0700

    implement new authentication strategy on track routes

--- server/app/routes/track.routes.js
+++ server/app/routes/track.routes.js
@@ -1,24 +1,24 @@
 const router = require('express').Router();
 const passport = require('passport');
 const	trackController = require('../controllers/track.controller');
+const authController = require('../controllers/auth.controller');
 const upload = require('../../config/storage_config');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
 // TODO: set max count for file uploads:
 // upload.array('audioFiles', < max count >)
 router.post('/', 
-	requireAuth, 
+	authController.requireAuth, 
 	upload.array('audioFiles'), 
 	trackController.handlePostTracks
 );
 
 router.put('/:trackId', 
-	requireAuth, 
+	authController.requireAuth, 
 	trackController.editTrack
 );
 
 router.delete('/:trackId', 
-	requireAuth, 
+	authController.requireAuth, 
 	trackController.removeTrack
 );
 
