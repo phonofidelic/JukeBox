b76e779045c50b4117efd94f2bcb6a4c2d728d1f
phonofidelic <chris_clemons85@hotmail.com>
2019-03-30 10:56:24 -0700

    implement new authentication strategy on library routes

--- server/app/routes/library.routes.js
+++ server/app/routes/library.routes.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
 const passport = require('passport');
 const	libraryController = require('../controllers/library.controller');
+const authController = require('../controllers/auth.controller');
 const upload = require('../../config/storage_config');
 
-const requireAuth = passport.authenticate('jwt', { session: false });
+router.get('/', 
+	authController.requireAuth, 
+	libraryController.loadLibrary
+);
 
-router.get('/', requireAuth, libraryController.loadLibrary);
-router.get('/artists/:artistId', requireAuth, libraryController.getArtist);
-router.get('/albums/:albumId', requireAuth, libraryController.getAlbum);
+router.get('/artists/:artistId', 
+	authController.requireAuth, 
+	libraryController.getArtist
+);
+
+router.get('/albums/:albumId', 
+	authController.requireAuth, 
+	libraryController.getAlbum
+);
 
 module.exports = router;
