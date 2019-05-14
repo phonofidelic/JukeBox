1142d1d2e8465912f4d2e0475d2fc419526cfd49
phonofidelic <chris_clemons85@hotmail.com>
2019-05-14 12:05:18 -0700

    comment out unused code

--- client/src/components/Player/QueueList.js
+++ client/src/components/Player/QueueList.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import {
 	ThemeContext,
 	getColorPrimaryMain,
-	getColorPrimaryLight,
+	// getColorPrimaryLight,
 	getSecondaryBackgroundColor,
 	getPlayerHeight,
 	getPlayerProgressOpenHeight,
