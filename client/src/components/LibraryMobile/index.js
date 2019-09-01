4a41ec0802f6342892c2c15b64872d40ca8976fa
phonofidelic <chris.clemons85@gmail.com>
2019-08-31 22:02:49 -0700

    fix scrolling issue in LibraryMobile

--- client/src/components/LibraryMobile/index.js
+++ client/src/components/LibraryMobile/index.js
@@ -10,73 +10,75 @@ import List from '@material-ui/core/List';
 import withTheme from '@material-ui/core/styles/withTheme';
 
 const ORDER_TYPES = {
-	TITLE: 'title',
-	ARTIST: 'artist',
-	ALBUM: 'album'
+  TITLE: 'title',
+  ARTIST: 'artist',
+  ALBUM: 'album'
 };
 
 export class LibraryMobile extends Component {
-	static contextType = ThemeContext;
+  static contextType = ThemeContext;
 
-	state = {
-		orderBy: ORDER_TYPES.TITLE,
-	}
+  state = {
+    orderBy: ORDER_TYPES.TITLE
+  };
 
-	setOrder(type) {
-		this.setState({
-			orderBy: type
-		});
-	}
+  setOrder(type) {
+    this.setState({
+      orderBy: type
+    });
+  }
 
-	render() {
-		const { 
-			library,
-			handleOrderBy,
-		} = this.props;
+  render() {
+    const { library, handleOrderBy, queueHasTracks } = this.props;
 
-		const theme = this.context;
+    const theme = this.context;
 
-		const styles = {
-			root: {
-				background: theme.palette.secondary.light,
-				borderTop: `1px solid ${theme.palette.primary.main}`,
-				padding: '0px',
-				// paddingTop: theme.dimensions.topNav.height + theme.dimensions.libraryControls.height,
-				paddingTop: theme.dimensions.libraryControls.height,
-				paddingBottom: theme.dimensions.player.height, // TODO: link value to Player + Nav height
-				position: 'fixed',
-				width: '100%',
-				top: 0,
-				bottom: 0,
-				overflowY: 'scroll',
-				// WebkitOverflowScrolling: 'touch', // BUG: Causes LibraryControls to jerk
-			}
-		};
+    const styles = {
+      root: {
+        background: theme.palette.secondary.light,
+        borderTop: `1px solid ${theme.palette.primary.main}`,
+        padding: '0px',
+        // paddingTop: theme.dimensions.topNav.height + theme.dimensions.libraryControls.height,
+        paddingTop: theme.dimensions.libraryControls.height,
+        paddingBottom: theme.dimensions.player.height // TODO: link value to Player + Nav height
+        // position: 'fixed',
+        // width: '100%',
+        // top: 0,
+        // bottom: 0,
+        // overflowY: 'scroll',
+        // WebkitOverflowScrolling: 'touch' // BUG: Causes LibraryControls to jerk
+      },
+      list: {
+        position: 'fixed',
+        width: '100%',
+        top: theme.dimensions.libraryControls.height,
+        bottom: queueHasTracks
+          ? theme.dimensions.player.height + theme.dimensions.navMobile.height
+          : theme.dimensions.player.height,
+        overflowY: 'scroll',
+        WebkitOverflowScrolling: 'touch' // BUG: Causes LibraryControls to jerk
+      }
+    };
 
-		return (
-			<div 
-				style={styles.root} 
-			>
-				<LibraryControls 
-					orderBy={this.state.orderBy} 
-					setOrder={this.setOrder.bind(this)} 
-					handleOrderBy={handleOrderBy}
-				/>
-				<List>
-				{
-					library.tracks.length > 0 ? library.tracks.map(track => (
-						<TrackListItemContainer 
-							key={track._id} 
-							track={track}
-						/>
-					))
-					:
-					(<Typography>No tracks in Library</Typography>)
-				}
-				</List>
-			</div>
-		)
-	}
+    return (
+      <div style={styles.root}>
+        <LibraryControls
+          orderBy={this.state.orderBy}
+          setOrder={this.setOrder.bind(this)}
+          handleOrderBy={handleOrderBy}
+        />
+        <List style={styles.list}>
+          {library.tracks.length > 0 ? (
+            library.tracks.map(track => (
+              <TrackListItemContainer key={track._id} track={track} />
+            ))
+          ) : (
+            <Typography>No tracks in Library</Typography>
+          )}
+        </List>
+      </div>
+    );
+  }
 }
 
 export default withTheme()(LibraryMobile);
