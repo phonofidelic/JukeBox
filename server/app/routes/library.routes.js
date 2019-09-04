8cce19383219c1f7cd6117380e41cef2d416d309
phonofidelic <chris.clemons85@gmail.com>
2019-09-03 23:37:10 -0700

    activate auth check

--- server/app/routes/library.routes.js
+++ server/app/routes/library.routes.js
@@ -1,22 +1,21 @@
 const router = require('express').Router();
 const passport = require('passport');
-const	libraryController = require('../controllers/library.controller');
+const libraryController = require('../controllers/library.controller');
 const authController = require('../controllers/auth.controller');
 const upload = require('../../config/storage_config');
 
-router.get('/', 
-	// authController.requireAuth, 
-	libraryController.loadLibrary
+router.get('/', authController.requireAuth, libraryController.loadLibrary);
+
+router.get(
+  '/artists/:artistId',
+  authController.requireAuth,
+  libraryController.getArtist
 );
 
-router.get('/artists/:artistId', 
-	authController.requireAuth, 
-	libraryController.getArtist
-);
-
-router.get('/albums/:albumId', 
-	authController.requireAuth, 
-	libraryController.getAlbum
+router.get(
+  '/albums/:albumId',
+  authController.requireAuth,
+  libraryController.getAlbum
 );
 
 module.exports = router;
