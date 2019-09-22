0fab7e005f577d69fed9a4f40cc74326403d0072
Christopher Clemons <chris.clemons85@gmail.com>
2019-09-21 20:02:57 -0700

    fix build for single-project setup

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,27 +1,24 @@
 // import createBrowserHistory from 'history/createBrowserHistory';
 // const createBrowserHistory = require("history").createBrowserHistory;
-import { createBrowserHistory } from 'history'
+import { createBrowserHistory } from 'history';
 
 export const history = createBrowserHistory();
 
-const API_ROOT = process.env.NODE_ENV === 'production' ? 
-  'http://ec2-35-164-241-235.us-west-2.compute.amazonaws.com'
-  :
-  '';
+const API_ROOT = process.env.NODE_ENV === 'production' ? '' : '';
 
 export const URLS = {
   LIBRARY_URL: `${API_ROOT}/api/library`,
-	TRACK_URL: `${API_ROOT}/api/tracks`,
+  TRACK_URL: `${API_ROOT}/api/tracks`,
   ARTIST_URL: `${API_ROOT}/api/library/artists`,
   ALBUM_URL: `${API_ROOT}/api/library/albums`,
   STREAM_URL: `${API_ROOT}/api/stream`,
   AUTH_URL: `${API_ROOT}/api/auth`,
-  GDRIVE_URL: `${API_ROOT}/api/gdrive`,
-}
+  GDRIVE_URL: `${API_ROOT}/api/gdrive`
+};
 
 // TODO: transistion to use theme.context instead
 export const THEME = {
-	typography: {
+  typography: {
     useNextVariants: true,
     // Use the system font over Roboto.
     fontFamily:
@@ -29,15 +26,15 @@ export const THEME = {
       '"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif',
     fontWeightMedium: 500,
     body1: {
-      fontWeight: 500,
+      fontWeight: 500
     },
     subheading: {
-      fontSize: 12,
+      fontSize: 12
     },
     button: {
       // fontStyle: 'italic',
       // size: 'large'
-    },
+    }
   },
   palette: {
     primary: {
@@ -45,7 +42,7 @@ export const THEME = {
       light: '#fff', // background
       dark: '#222',
       hover: '#ededed',
-      selected: '#dbdbdb',
+      selected: '#dbdbdb'
     },
     secondary: {
       main: '#e91e00', // accent
@@ -61,36 +58,36 @@ export const THEME = {
   },
   dimensions: {
     libraryDesktop: {
-      maxWidth: 900,
+      maxWidth: 900
     },
     libraryControls: {
-      height: 32,
+      height: 32
     },
     nav: {
-      navHeight: 56,
+      navHeight: 56
     },
     navDesktop: {
       navWidth: 80, // TODO: Find usage of this and change to 'width'
       width: 80,
       marginTop: 58,
-      zIndex: 3,
+      zIndex: 3
     },
     navMobile: {
       // height: 56,
       height: 0,
-      zIndex: 3,
+      zIndex: 3
     },
     topNav: {
-      height: 56,
+      height: 56
       // height: 30,
       // height: 0,
     },
     player: {
       width: 900,
-      height: 58,
+      height: 58
     },
     playerProgress: {
-      height: 10,
+      height: 10
     }
-  },
-}
+  }
+};
--- client/src/utils/idbUtils.js
+++ client/src/utils/idbUtils.js
@@ -1,41 +1,39 @@
 import { openDB } from 'idb';
 
-const IDB_VERSION = 1;
+const IDB_VERSION = 2;
 
 const db = openDB('jukebox', IDB_VERSION, {
-	upgrade(db) {
-		console.log('upgrade db')
-		const store = db.createObjectStore('tracks', {
-			keyPath: '_id'
-		});
-		store.createIndex('title', 'title')
-	}
+  upgrade(db) {
+    console.log('upgrade db');
+    const store = db.createObjectStore('tracks', {
+      keyPath: '_id'
+    });
+    store.createIndex('title', 'title');
+  }
 });
 
 export const idbTrack = {
-	getAll: async () => {
-		console.log('idbTrack.getAll')
-		return (await db).getAllFromIndex('tracks', 'title');
-	},
-	addMany: async data => {
+  getAll: async () => {
+    console.log('idbTrack.getAll');
+    return (await db).getAllFromIndex('tracks', 'title');
+  },
+  addMany: async data => {
     const tx = (await db).transaction('tracks', 'readwrite');
     data.forEach(item => {
       tx.store.add(item);
     });
     await tx.done;
-	},
-	put: async data => {
-		const tx = (await db).transaction('tracks', 'readwrite');
-		tx.store.put(data);
-		await tx.done;
-	},
-	delete: async id => {
-		const tx = (await db).transaction('tracks', 'readwrite');
-		tx.store.delete(id);
-		await tx.done;
-	}
+  },
+  put: async data => {
+    const tx = (await db).transaction('tracks', 'readwrite');
+    tx.store.put(data);
+    await tx.done;
+  },
+  delete: async id => {
+    const tx = (await db).transaction('tracks', 'readwrite');
+    tx.store.delete(id);
+    await tx.done;
+  }
 };
 
-export const clearIdb = () => {
-
-}
+export const clearIdb = () => {};
