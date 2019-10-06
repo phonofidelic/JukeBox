ca5af23ae33a4bea6a3fa557195464478faefcca
Christopher Clemons <chris.clemons85@gmail.com>
2019-10-07 01:36:25 +0200

    fix loadLibrary function
    Update load library to destructure userId off of req object.

--- server/app/controllers/library.controller.js
+++ server/app/controllers/library.controller.js
@@ -4,40 +4,38 @@ const Album = require('../models/album.model');
 const utils = require('./utils');
 
 module.exports.loadLibrary = (req, res, next) => {
-	const userId = req.get('userId');
-	utils.loadTracks(Track, userId)
-	.then((library) => {
-		res.json({ message: 'Library loaded', library: library });
-	}).catch(err => {
-		next(err);
-	});
+  const { userId } = req;
+  utils
+    .loadTracks(Track, userId)
+    .then(library => {
+      res.json({ message: 'Library loaded', library: library });
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 module.exports.getArtist = (req, res, next) => {
-	const userId = req.get('userId');
-
-	Artist.findById(req.params.artistId)
-	.then(artist => {
-		if (!artist) return next(new Error('Artist document not found'));
-		console.log('### getArtist, artist:', artist);
-		res.json({ message: 'Artist details loaded', artist: artist });
-	})
-	.catch(err => {
-		console.error('### getArtist error:', err)
-		next({error: new Error(err)});
-	});
+  Artist.findById(req.params.artistId)
+    .then(artist => {
+      if (!artist) return next(new Error('Artist document not found'));
+      console.log('### getArtist, artist:', artist);
+      res.json({ message: 'Artist details loaded', artist: artist });
+    })
+    .catch(err => {
+      console.error('### getArtist error:', err);
+      next({ error: new Error(err) });
+    });
 };
 
 module.exports.getAlbum = (req, res, next) => {
-	const userId = req.get('userId');
-
-	Album.findById(req.params.albumId)
-	.then(album => {
-		if (!album) return next(new Error('Album document not found'));
-		console.log('### getArtist, album:', album);
-		res.json({ message: 'Album details loaded', album: album });
-	})
-	.catch(err => {
-		next(err);
-	});
-}
+  Album.findById(req.params.albumId)
+    .then(album => {
+      if (!album) return next(new Error('Album document not found'));
+      console.log('### getArtist, album:', album);
+      res.json({ message: 'Album details loaded', album: album });
+    })
+    .catch(err => {
+      next(err);
+    });
+};
