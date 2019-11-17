817993bc15c3913803b024622466dc4df9c0e7e7
Christopher Clemons <chris.clemons85@gmail.com>
2019-11-17 12:15:00 -0800

    fix ci config typo

--- server/config/ci.js
+++ server/config/ci.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
   // API_ROOT: 'http://localhost:3000',
   PORT: 3000,
-  DB_CONNECTION: 'mongodb://127.0.0.1:27017/jukebox_ci',
-  CLIENT_DIR: '../client/build',
-  FS_AUDIO: 'uploads/audio',
-  FS_IMAGE: 'uploads/images',
-  TMP: 'tmp',
+  DB_CONNECTION: "mongodb://127.0.0.1:27017/jukebox_ci",
+  CLIENT_DIR: "../client/build",
+  FS_AUDIO: "uploads/audio",
+  FS_IMAGE: "uploads/images",
+  TMP: "tmp",
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXP: '15m',
-  JWT_AUD: 'jukebox_client',
-  JWT_ISS: 'jukebox_api',
+  JWT_EXP: "15m",
+  JWT_AUD: "jukebox_client",
+  JWT_ISS: "jukebox_api",
   DISCOGS_TOKEN: process.env.DISCOGS_TOKEN,
   // G_CLIENT_ID:
   //   '1055271812237-icje6bl0lorut3pseo494avkdn6ohgub.apps.googleusercontent.com',
@@ -18,7 +18,7 @@ module.exports = {
   // G_REDIRECT_URI: 'api/gdrive/authcode',
   S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
   S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
-  S3_BUCKET_NAME: 'jukebox-ci-storage',
-  STORAGE_BASE_URL: process.env.STORAGE_BASE_URL
+  S3_BUCKET_NAME: "jukebox-ci-storage",
+  STORAGE_BASE_URL: process.env.STORAGE_BASE_URL,
   STORAGE_MAX: 16106127360
 };
